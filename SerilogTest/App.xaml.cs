b3bff8f339ca672f6bceb76792896c2ca95018e0
jenni <jenni@G>
2022-07-06 19:02:13 -0400

    Add project files.

---
+++ SerilogTest/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Serilog;
+using Serilog.Formatting.Compact;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SerilogTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        
+        protected override void OnStartup(StartupEventArgs args)
+        {
+            // Setup Serilog 
+            using var log = new LoggerConfiguration()
+        .WriteTo.Debug()
+        .WriteTo.File("logs/AppLog.txt", rollingInterval: RollingInterval.Day)
+        //.WriteTo.File(new CompactJsonFormatter(), "log.txt")
+        .CreateLogger();
+            
+            // This is what a log message looks like.
+            log.Information("App Startup");
+
+            // Make the MainWindow and pass in the serilog
+            MainWindow mainWindows = new MainWindow(log);
+            mainWindows.Show();
+            
+            // Call the base method that we overrode
+            base.OnStartup(args);
+        }
+    }
+}
---
+++ SerilogTest/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SerilogTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Take the created Serilog Instance as a parameter.
+        public MainWindow(ILogger logger)
+        {
+            // Create local name for the Serilog we passed in.
+            Log.Logger = logger;
+            
+            // Write a log message.
+            Log.Information("The global logger has been configured");
+            
+            // Finish setting up the window.
+            InitializeComponent();
+        }
+    }
+}
